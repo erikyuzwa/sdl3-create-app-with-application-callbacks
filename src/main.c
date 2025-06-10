c5c20c9f97d459b2db0572f961d70068f3693b1c
erikyuzwa <erikyuzwa@gmail.com>
2025-06-09 18:37:10 -0600

    commit the code!

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 
-#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -21,7 +21,7 @@ static const struct
 typedef struct {
     SDL_Window* window;
     SDL_Renderer* renderer;
-    bool running;
+    bool is_running;
 } AppState;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
@@ -45,7 +45,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
         return SDL_APP_FAILURE;
     }
 
-    AppState* state = (AppState *)SDL_calloc(1, sizeof(AppState));
+    AppState* state = (AppState*)SDL_calloc(1, sizeof(AppState));
     if (!state) return SDL_APP_FAILURE;
 
     SDL_CreateWindowAndRenderer("SDL3 App Callback Window", 800, 600, SDL_WINDOW_RESIZABLE, &state->window, &state->renderer);
@@ -55,11 +55,10 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
         return SDL_APP_FAILURE;
     }
 
-
-    state->running = true;
+    state->is_running = true;
 
     *appstate = state;
-    
+
     return SDL_APP_CONTINUE;
 }
 
@@ -69,13 +68,18 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 
     switch (event->type) {
     case SDL_EVENT_QUIT:
-        state->running = false;
+        state->is_running = false;
+        break;
+
+    case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+        SDL_Log("Window close requested");
+        state->is_running = false;
         break;
 
     case SDL_EVENT_KEY_DOWN:
         if (event->key.key == SDLK_ESCAPE) {
             SDL_Log("ESC pressed, exiting");
-            state->running = false;
+            state->is_running = false;
         }
         else {
             SDL_Log("Key: %s", SDL_GetKeyName(event->key.key));
@@ -84,11 +88,6 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 
     case SDL_EVENT_WINDOW_RESIZED:
         SDL_Log("Window Resized to %d x %d", event->window.data1, event->window.data2);
-        break;
-
-    case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-        SDL_Log("Window close requested");
-        state->running = false;
         break;
 
     default:
@@ -106,11 +105,11 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
     SDL_SetRenderDrawColor(state->renderer, 40, 40, 60, 255);
     SDL_RenderClear(state->renderer);
 
-    // Render your game content here...
+    // do rendering stuff here
 
     SDL_RenderPresent(state->renderer);
 
-    return state->running ? SDL_APP_CONTINUE : SDL_APP_SUCCESS; // return SDL_APP_SUCCESS to quit
+    return state->is_running ? SDL_APP_CONTINUE : SDL_APP_SUCCESS; // return SDL_APP_SUCCESS to quit
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
